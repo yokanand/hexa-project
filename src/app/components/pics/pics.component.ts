9cad4e00bbf36d6b23c7c623e4f599b3a98d85eb
yokanandCIC <yokanand.thirupathi-cic.germany@ibm.com>
2018-06-05 17:28:15 +0200

    Created pics module

---
+++ src/app/components/pics/pics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pics',
+  templateUrl: './pics.component.html',
+  styleUrls: ['./pics.component.css']
+})
+export class PicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
